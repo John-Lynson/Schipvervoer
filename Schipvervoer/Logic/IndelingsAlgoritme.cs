8166fbc71b8aa00716df35f58f8f44d7762f0115
Johnny Graanoogst <Johnlynson809@gmail.com>
2024-01-09 21:36:31 +0100

    aanpassing

--- Schipvervoer/Logic/IndelingsAlgoritme.cs
+++ Schipvervoer/Logic/IndelingsAlgoritme.cs
@@ -11,8 +11,17 @@ namespace Schipvervoer.Logic
     {
         public void IndeelContainers(Schip schip, List<Container> containers)
         {
-            // Basis logica voor containerindeling
-            // Bijvoorbeeld: eerst gekoelde containers, dan waardevolle containers, en dan de rest
+            // Sorteer containers: eerst gekoeld, dan waardevol, dan overige
+            var gesorteerdeContainers = containers
+                .OrderByDescending(c => c.MoetGekoeldWorden)
+                .ThenByDescending(c => c.IsWaardeVol)
+                .ToList();
+
+            foreach (var container in gesorteerdeContainers)
+            {
+                // Voeg toe aan het schip, controleer op gewicht en type
+                schip.VoegContainerToe(container);
+            }
         }
     }
-
+}
